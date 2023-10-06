60edeff0eea40873d1579aff05566eb605fd8418
Hala Qitouqa <h.qitouqa@gmail.com>
2023-10-06 23:02:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+/*function findMatching(drivers,string){
+    drivers.filter(function(value){
+        return value === string
+    })
+}
+
+console.log(findMatching(drivers, "Bobby"))
+*/
+function findMatching(drivers, string){
+
+ return drivers.filter(function(driver){
+    if(driver.toUpperCase() == string.toUpperCase()){
+        return true
+    }
+})
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(driver){
+        let l=string.length
+        if(driver.charAt()===string){
+            return true
+        }
+    })
+}
+
+function matchName(drivers, string){
+    return drivers.filter(function(driver){
+        if(driver.name===string){
+            return driver
+        }
+    })
+}
